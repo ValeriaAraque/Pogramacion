c2f04aa51cc2789343b3890bab05ce256d637402
ValeriaAraque <valeriaraquec@gmail.com>
2020-08-30 16:14:31 -0500

    Ciclos for

---
+++ Clases/sextaclase.py
@@ -0,0 +1,10 @@
+# Ciclos for
+for repeticion in range (10):
+    print (repeticion)
+print ("Hasta luego")
+
+preguntaNumero = "Por favor ingresa un numero entero para saber hasta donde debo contar : "
+numero = int(input(preguntaNumero))
+
+for i in range(numero):
+    print (i+1)
